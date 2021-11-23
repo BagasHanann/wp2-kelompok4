19752dc61d25e3926fe6716bacd9b7698f67660b
Bagas Hananto <87441334+BagasHanann@users.noreply.github.com>
2021-11-23 22:35:51 +0700

    Menambahkan session di login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,14 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller 
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->library('form_validation');
-    }
-
     public function index()
     {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
         $data['title'] = 'User Login';
         $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
