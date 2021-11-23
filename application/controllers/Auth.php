8bf2f99270703136ca4d4091ac748c7a61b44372
Bagas Hananto <87441334+BagasHanann@users.noreply.github.com>
2021-11-23 22:31:48 +0700

    Update Auth.php
    Menghapus function registrasi, jadi hanya 1 user yang login.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -27,68 +27,23 @@ class Auth extends CI_Controller
     {
        $email = $this->input->post('email');
        $password = $this->input->post('password');
-       $user = $this->db->get_where('user', ['email' => $email ])->row_array();   
+       $user = $this->db->get_where('user', ['email' => $email ])->row_array();  
        if($user) {
           if ($user['is_active'] == 1) {
-            if (password_verify($password, $user['password'])) {
-                $data = [
-                    'email' => $user['email'],
-                    'role_id' => $user['role_id']
-                ]; 
-                $this->session->set_userdata($data);
-                if ($user['role_id'] == 1){
-                    redirect('admin');
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ]; 
+                    $this->session->set_userdata($data);
+                    if ($user['role_id'] == 1){
+                        redirect('admin');
+                    }
                 } else {
-                redirect('user');
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password</div>');
+                redirect('auth');
                 }
-            } else {
-            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password</div>');
-            redirect('auth');
             }
-        } else {
-            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email not been activated</div>');
-            redirect('auth');
-          }
-       } else {
-           $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered</div>');
-           redirect('auth');
-       }
-    } 
-
-    public function registrasi()
-    {
-        if ($this->session->userdata('email')) {
-            redirect('user');
-        }
-        $data['title'] = 'User Registration';
-        $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
-            'is_unique' => 'This email has already registered'
-        ]);
-        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[4]|matches[password2]', [
-            'matches' => 'Password dont matches',
-            'min_length' => 'Password to short'
-        ]);
-        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
-
-        if( $this->form_validation->run() == false ) {
-            $this->load->view('templates/auth_header', $data);
-            $this->load->view('auth/registrasi');
-            $this->load->view('templates/auth_footer');
-        } else {
-           $data = [
-               'name' => $this->input->post('name', true),
-               'email' => $this->input->post('email', true),
-               'image' => 'default.jpg',
-               'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
-               'role_id' => 2,
-               'is_active' => 1,
-               'date_created' => time()
-           ];
-           $this->db->insert('user', $data);
-           $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your account has been created</div>');
-           redirect('auth');
         }
     }
-
 }
