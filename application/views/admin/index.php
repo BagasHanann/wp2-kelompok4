2c9b6b006db639225e8d645cfd5a3822214bbc34
destiansf <destiannisafirti@gmail.com>
2021-11-29 18:47:42 +0700

    menambahkan views admin

---
+++ application/views/admin/index.php
@@ -0,0 +1,28 @@
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+<div class="row">
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            Jumlah Data Mahasiswa
+                        </div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
