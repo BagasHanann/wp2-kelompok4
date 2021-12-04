b8dfee08c80bc28104c5e9e0e6b44e856d8926fa
BagasHanann <bagashp01@gmail.com>
2021-12-04 18:08:14 +0700

    Modified autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('email', 'session', 'database', 'form_validation', 'mahasiswa');
+$autoload['libraries'] = array('email', 'session', 'database', 'form_validation', 'pagination');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'security');
+$autoload['helper'] = array('url', 'file', 'security', 'mahasiswa');
 
 /*
 | -------------------------------------------------------------------
